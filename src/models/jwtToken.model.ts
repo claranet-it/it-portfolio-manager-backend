d9cbc9e0384eb23e84a6a299f874ad71278086d6
Massimo Biagioli <biagiolimassimo@gmail.com>
2023-10-02 14:15:02 +0200

    Fix user info

--- src/models/jwtToken.model.ts
+++ src/models/jwtToken.model.ts
@@ -1,9 +1,9 @@
 import { Static, Type } from '@sinclair/typebox'
 
 export const JwtToken = Type.Object({
-  'email': Type.String(),
-  'name': Type.String(),
-  'picture': Type.String(),
+  email: Type.String(),
+  name: Type.String(),
+  picture: Type.String(),
 })
 
 export type JwtTokenType = Static<typeof JwtToken>
