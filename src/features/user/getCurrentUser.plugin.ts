9e21b38f87c10efd033491a4d11a5761e57c8816
Massimo Biagioli <biagiolimassimo@gmail.com>
2023-10-04 12:50:22 +0200

    Plugin Refactor

--- src/features/user/getCurrentUser.plugin.ts
+++ src/features/user/getCurrentUser.plugin.ts
@@ -1,8 +1,7 @@
 import fp from 'fastify-plugin'
 import { FastifyInstance } from 'fastify'
 import { JwtTokenType } from '@models/jwtToken.model'
-import { UserProfileType, UserWithProfileType } from '@models/user.model'
-import { QueryCommand } from '@aws-sdk/client-dynamodb'
+import { UserWithProfileType } from '@models/user.model'
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -11,27 +10,10 @@ declare module 'fastify' {
 }
 
 async function getCurrentUserPlugin(fastify: FastifyInstance): Promise<void> {
-  const getUserProfile = async (
-    uid: string,
-  ): Promise<UserProfileType | null> => {
-    const command = new QueryCommand({
-      TableName: fastify.getTableName('UserProfile'),
-      KeyConditionExpression: 'uid = :uid',
-      ExpressionAttributeValues: { ':uid': { S: uid } },
-    })
-    const result = await fastify.dynamoDBClient.send(command)
-    if (result?.Items?.length === 1 && result?.Items[0]?.crew?.S) {
-      return {
-        crew: result.Items[0].crew?.S ?? '',
-      }
-    }
-    return null
-  }
-
   const getCurrentUser = async (
     jwtToken: JwtTokenType,
   ): Promise<UserWithProfileType> => {
-    const userProfile = await getUserProfile(jwtToken.email)
+    const userProfile = await fastify.getUserProfile(jwtToken.email)
     if (!userProfile) {
       return {
         ...jwtToken,
