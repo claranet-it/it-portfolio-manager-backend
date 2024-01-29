19875cdad05088559e9eb2d59bef4c58f6142aa5
MauroMonteneri <107854959+MauroMonteneri@users.noreply.github.com>
2024-01-29 16:06:20 +0100

    fix call params

--- src/core/OpenAI/service/OpenAIService.ts
+++ src/core/OpenAI/service/OpenAIService.ts
@@ -4,13 +4,13 @@ import { openAiResponseType } from "../model/openAI";
 export class OpenAIService{
     async getResponse(prompt: string): Promise<openAiResponseType>{
         const openai = new OpenAI();
-        const completion = await openai.completions.create({
+        const completion = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
-            prompt: prompt,
+            messages: [{role: 'user', content: prompt}],
             temperature: 0
         })
         return{
-            message: completion.choices[0].text
+            message: completion.choices[0].message.content ?? ''
         }
     }
 }
