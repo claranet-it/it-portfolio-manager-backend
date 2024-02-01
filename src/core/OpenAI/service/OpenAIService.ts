d9fa51897245da1232b06d360b9ce04c58564a54
MauroMonteneri <107854959+MauroMonteneri@users.noreply.github.com>
2024-02-01 15:56:37 +0100

    wip read ssm parameter

--- src/core/OpenAI/service/OpenAIService.ts
+++ src/core/OpenAI/service/OpenAIService.ts
@@ -1,9 +1,12 @@
 import OpenAI from "openai";
 import { openAiResponseType } from "../model/openAI";
+import { SSM } from "@aws-sdk/client-ssm";
 
 export class OpenAIService{
     async getResponse(prompt: string): Promise<openAiResponseType>{
-        const openai = new OpenAI();
+        const ssmClient = new SSM();
+        const key = await ssmClient.getParameter({Name: process.env.OPENAI_API_KEY_ARN, WithDecryption: true});
+        const openai = new OpenAI({apiKey: key.Parameter?.Value});
         const completion = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             messages: [{role: 'user', content: prompt}],
