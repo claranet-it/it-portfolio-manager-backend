b34a69851072b7e2c353bc8e08d0cbd05b332dcf
MauroMonteneri <107854959+MauroMonteneri@users.noreply.github.com>
2024-02-20 17:15:51 +0100

    restore original package and db connection

--- src/infrastructure/db/DynamoDBConnection.ts
+++ src/infrastructure/db/DynamoDBConnection.ts
@@ -3,16 +3,16 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 export class DynamoDBConnection {
   static getClient(): DynamoDBClient {
     let dynamoDbClientOptions = {}
-    // if (process.env.IS_OFFLINE) {
-    //   dynamoDbClientOptions = {
-    //     region: 'localhost',
-    //     endpoint: 'http://0.0.0.0:8000',
-    //     credentials: {
-    //       accessKeyId: 'MockAccessKeyId',
-    //       secretAccessKey: 'MockSecretAccessKey',
-    //     },
-    //   }
-    // }
+    if (process.env.IS_OFFLINE) {
+      dynamoDbClientOptions = {
+        region: 'localhost',
+        endpoint: 'http://0.0.0.0:8000',
+        credentials: {
+          accessKeyId: 'MockAccessKeyId',
+          secretAccessKey: 'MockSecretAccessKey',
+        },
+      }
+    }
 
     return new DynamoDBClient(dynamoDbClientOptions)
   }
