00fbf7392215abec7d616a8acb76851f3e03ad18
MauroMonteneri <107854959+MauroMonteneri@users.noreply.github.com>
2024-02-21 16:35:13 +0100

    add missing test

--- test/unit/User/runner/removeResignedPeopleRunner.test.ts
+++ test/unit/User/runner/removeResignedPeopleRunner.test.ts
@@ -101,3 +101,37 @@ test('not remove user if account not found in Slack', async (t) => {
     t.equal(removedPeople.length,0)
 })
 
+test('remove account with it.clara.net in slack', async (t) => {
+    const email = 'resigned@claranet.com'
+    const getAllUserProfiles = () =>
+      Promise.resolve([
+        {
+          uid: email,
+          crew: 'moon',
+          company: 'it',
+          name: 'resigned',
+        },
+      ])
+    const getSlackAccountStatuses = () =>
+      Promise.resolve([
+        {
+          email: 'resigned@it.clara.net',
+          active: false,
+        },
+      ])
+    const removedPeople: string[] = []
+    const removeResigned = (uid: string) => {
+      removedPeople.push(uid)
+      return Promise.resolve()
+    }
+  
+    await removeResignedPeople(
+      getAllUserProfiles,
+      getSlackAccountStatuses,
+      removeResigned,
+    )
+  
+    t.equal(removedPeople.length,1)
+    t.equal(removedPeople[0], email)
+})
+
