eabc00bbb0c54f50be8056121a500991ea35c075
MauroMonteneri <107854959+MauroMonteneri@users.noreply.github.com>
2024-05-24 09:37:24 +0200

    fix google client id and secret in local env

--- src/infrastructure/SSM/DummySSMClient.ts
+++ src/infrastructure/SSM/DummySSMClient.ts
@@ -2,10 +2,10 @@ import { SSMClientInterface } from '@src/core/SSM/SSMClientInterface'
 
 export class DummySSMClient implements SSMClientInterface {
   getGoogleClientId(): Promise<string> {
-    return Promise.resolve(process.env.GOOGLE_CLIENT_ID ?? '')
+    return Promise.resolve(process.env.GOOGLE_CLIENT_ID_ARN ?? '')
   }
   getGoogleSecret(): Promise<string> {
-    return Promise.resolve(process.env.GOOGLE_CLIENT_SECRET ?? '')
+    return Promise.resolve(process.env.GOOGLE_CLIENT_SECRET_ARN ?? '')
   }
   async getOpenAIkey(): Promise<string> {
     return Promise.resolve('dummyOpenAIApiKey')
