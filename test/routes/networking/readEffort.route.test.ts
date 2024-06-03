b31e25e697a4376a42e1827393920607af0097f7
Micol Panetta <micol.panetta@it.clara.net>
2024-06-03 10:52:07 +0200

    add test

---
+++ test/routes/networking/readEffort.route.test.ts
@@ -0,0 +1,49 @@
+import { afterEach, beforeEach, test } from 'tap'
+import createApp from '@src/app'
+import { FastifyInstance } from 'fastify'
+
+let app: FastifyInstance
+
+function getToken(company: string): string {
+  return app.createTestJwt({
+    email: 'nicholas.crow@email.com',
+    name: 'Nicholas Crow',
+    picture: 'https://test.com/nicholas.crow.jpg',
+    company: company,
+  })
+}
+
+beforeEach(async () => {
+  app = createApp({ logger: false })
+  await app.ready()
+})
+
+afterEach(async () => {
+  await app.close()
+})
+
+test('read networking effort without authentication', async (t) => {
+  const response = await app.inject({
+    method: 'GET',
+    url: '/api/networking/effort',
+  })
+
+  t.equal(response.statusCode, 401)
+})
+
+test('read company networking effort', async (t) => {
+  const company = 'test company'
+  const token = getToken(company)
+  const response = await app.inject({
+    method: 'GET',
+    url: '/api/networking/effort',
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  })
+
+  t.equal(response.statusCode, 200)
+
+  const expected =  "[{\"company\":\"test company\",\"effort\":[]}]"
+  t.same(response.payload, expected)
+})
