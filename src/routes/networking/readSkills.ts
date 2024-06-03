4935a40e1c3b9f930f2a248a423f34274f646345
Micol Panetta <micol.panetta@it.clara.net>
2024-06-03 09:45:40 +0200

    rename file

--- src/routes/networking/readSkills.ts
+++ src/routes/networking/readSkills.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import {NetworkingSkillsResponse, NetworkingSkillsResponseType} from "@src/core/Networking/model/networking.model";
 
-
 export default async function (fastify: FastifyInstance): Promise<void> {
   fastify.get<{
     Reply: NetworkingSkillsResponseType
