2004c7dc37c2cf9e1dc978fd0bc269e668f6bff1
MauroMonteneri <107854959+MauroMonteneri@users.noreply.github.com>
2024-06-06 12:19:25 +0200

    get jwt keys from ssm

--- src/infrastructure/SSM/DummySSMClient.ts
+++ src/infrastructure/SSM/DummySSMClient.ts
@@ -1,6 +1,12 @@
 import { SSMClientInterface } from '@src/core/SSM/SSMClientInterface'
 
 export class DummySSMClient implements SSMClientInterface {
+  getJwtPrivateKey(): Promise<string> {
+    throw new Error('Method not implemented.')
+  }
+  getJWTPulicKey(): Promise<string> {
+    throw new Error('Method not implemented.')
+  }
   getGoogleClientId(): Promise<string> {
     return Promise.resolve(process.env.GOOGLE_CLIENT_ID_ARN ?? '')
   }
@@ -8,9 +14,9 @@ export class DummySSMClient implements SSMClientInterface {
     return Promise.resolve(process.env.GOOGLE_CLIENT_SECRET_ARN ?? '')
   }
   async getOpenAIkey(): Promise<string> {
-    return Promise.resolve('dummyOpenAIApiKey')
+    return Promise.resolve('')
   }
   async getSlackToken(): Promise<string> {
-    return Promise.resolve('dummySlackToken')
+    return Promise.resolve('')
   }
 }
