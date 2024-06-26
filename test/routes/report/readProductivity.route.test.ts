c152107a9f66b89d8ccc61062e46c2370b9ab10e
Micol Panetta <micol.panetta@it.clara.net>
2024-06-26 10:04:34 +0200

    add 1 day test with 100% hours

--- test/routes/report/readProductivity.route.test.ts
+++ test/routes/report/readProductivity.route.test.ts
@@ -130,7 +130,7 @@ test('read productivity report: no working day', async (t) => {
     t.same(result, expected)
 })
 
-test('read productivity report', async (t) => {
+test('read productivity report 1 month', async (t) => {
     const company = 'it'
     const token = getToken(company)
     const response = await app.inject({
@@ -150,14 +150,14 @@ test('read productivity report', async (t) => {
                 "name":"Micol Panetta",
                 "picture":"picture-url"
             },
-            "workedHours":8,
+            "workedHours":12,
             "totalTracked":{
-                "billableProductivity":2,
+                "billableProductivity":3,
                 "nonBillableProductivity":0,
                 "slackTime":2,
-                "absence":0
+                "absence":1
             },
-            "totalProductivity":2
+            "totalProductivity":3
         },
         {
             "user":{
@@ -180,14 +180,93 @@ test('read productivity report', async (t) => {
                 "name":"Nicholas Crow",
                 "picture":"picture-url"
             },
-            "workedHours":4,
+            "workedHours":8,
             "totalTracked":{
-                "billableProductivity":1,
+                "billableProductivity":2,
                 "nonBillableProductivity":0,
-                "slackTime":1,
+                "slackTime":2,
                 "absence":0
             },
-            "totalProductivity":1
+            "totalProductivity":2
+        },
+        {
+            "user":{
+                "email":"testIt@test.com",
+                "name":"test italian",
+                "picture":""
+            },
+            "workedHours":0,
+            "totalTracked":{
+                "billableProductivity":0,
+                "nonBillableProductivity":0,
+                "slackTime":0,
+                "absence":0
+            },
+            "totalProductivity":0
+        }
+    ]
+
+    t.same(result, expected)
+})
+
+test('read productivity report 1 day', async (t) => {
+    const company = 'it'
+    const token = getToken(company)
+    const response = await app.inject({
+        method: 'GET',
+        url: '/api/report/productivity?from=2024-01-01&to=2024-01-01',
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+    })
+
+    t.equal(response.statusCode, 200)
+    const result = response.json<ProductivityReportResponseType>();
+    const expected = [
+        {
+            "user":{
+                "email":"micol.ts@email.com",
+                "name":"Micol Panetta",
+                "picture":"picture-url"
+            },
+            "workedHours":8,
+            "totalTracked":{
+                "billableProductivity":50,
+                "nonBillableProductivity":0,
+                "slackTime":25,
+                "absence":25
+            },
+            "totalProductivity":50
+        },
+        {
+            "user":{
+                "email":"george.python@email.com",
+                "name":"George Python",
+                "picture":""
+            },
+            "workedHours":0,
+            "totalTracked":{
+                "billableProductivity":0,
+                "nonBillableProductivity":0,
+                "slackTime":0,
+                "absence":0
+            },
+            "totalProductivity":0
+        },
+        {
+            "user":{
+                "email":"nicholas.crow@email.com",
+                "name":"Nicholas Crow",
+                "picture":"picture-url"
+            },
+            "workedHours":8,
+            "totalTracked":{
+                "billableProductivity":50,
+                "nonBillableProductivity":0,
+                "slackTime":50,
+                "absence":0
+            },
+            "totalProductivity":50
         },
         {
             "user":{
