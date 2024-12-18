6ff8c5e371a9fee871c63b568f8819ffd46ea66b
Nicola Corvo <136686724+nicolacorvo@users.noreply.github.com>
2024-12-18 15:15:08 +0100

    Protected /task/task-properties (#70)

--- src/routes/task/updateTaskProperties.ts
+++ src/routes/task/updateTaskProperties.ts
@@ -12,6 +12,12 @@ export default async function (fastify: FastifyInstance): Promise<void> {
     '/task-properties',
     {
       onRequest: [fastify.authenticate],
+      casbin: {
+        rest: {
+          getObj: 'task',
+          getAct: 'write',
+        },
+      },
       schema: {
         tags: ['Task'],
         body: TaskPropertiesUpdateQueryParams,
