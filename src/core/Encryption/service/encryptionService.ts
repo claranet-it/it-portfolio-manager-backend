8e4a12ffe5a53483b5c271268bd32aacbacccf06
paolorabbito <paolorabbito@gmail.com>
2025-05-19 11:37:40 +0200

    add: time entries in get request

--- src/core/Encryption/service/encryptionService.ts
+++ src/core/Encryption/service/encryptionService.ts
@@ -3,10 +3,13 @@ import { JwtTokenType } from '@src/core/JwtToken/model/jwtToken.model'
 import { NotFoundException } from '@src/shared/exceptions/NotFoundException'
 import { CompanyRepositoryInterface } from '@src/core/Company/repository/CompanyRepositoryInterface'
 import { CustomerType, ProjectToEncryptType, TaskType } from '@src/core/Task/model/task.model'
+import { TimeEntryRepositoryInterface } from '@src/core/TimeEntry/repository/TimeEntryRepositoryInterface'
+import { TimeEntriesToEncryptType } from '@src/core/TimeEntry/model/timeEntry.model'
 
 export class EncryptionService {
   constructor(
     private taskRepository: TaskRepositoryInterface,
+    private timeEntryRepository: TimeEntryRepositoryInterface,
     private companyRepository: CompanyRepositoryInterface
   ) {}
 
@@ -22,11 +25,17 @@ export class EncryptionService {
     const customers: CustomerType[] = await this.taskRepository.getCustomersByCompany(company.name);
     const projects = await this.taskRepository.getProjectsByCompany(company.id);
     const tasks: TaskType[] = await this.taskRepository.getTasksByCompany(company.name);
+    const timeEntries = await this.timeEntryRepository.getTimeEntriesByCompany(company.name);
 
-    return this.aggregateDataToEncrypt(tasks, customers, projects);
+    return this.aggregateDataToEncrypt(tasks, customers, projects, timeEntries);
   }
 
-  private aggregateDataToEncrypt(tasks: TaskType[], customers: CustomerType[], projects: ProjectToEncryptType[]): any {
+  private aggregateDataToEncrypt(
+    tasks: TaskType[],
+    customers: CustomerType[],
+    projects: ProjectToEncryptType[],
+    timeEntries: TimeEntriesToEncryptType[]
+  ): any {
     return {
       tasks: tasks.map((t) => ({
         id: t.id,
@@ -37,6 +46,7 @@ export class EncryptionService {
         name: c.name,
       })),
       projects: projects.map((p) => ({ id: p.id, name: p.name })),
+      timeEntries: timeEntries,
     }
   }
 }
