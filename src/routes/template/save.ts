14835d4d561fc7e3a5decc51049f09e1c91294d2
mariateresagraziano <marytex.dev@gmail.com>
2025-05-22 10:47:11 +0200

    fix field to create template

--- src/routes/template/save.ts
+++ src/routes/template/save.ts
@@ -43,7 +43,7 @@ export default async function (fastify: FastifyInstance): Promise<void> {
                 await fastify
                     .dependencyInjectionContainer()
                     .resolve('templateService')
-                    .save({ ...request.body, userEmail: request.user.email })
+                    .save({ ...request.body, userEmail: request.user.email, company: request.user.company })
                 return reply.send(JSON.stringify({ message: 'OK' }))
             } catch (error) {
                 request.log.error(error)
